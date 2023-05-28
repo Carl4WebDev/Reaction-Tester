b91a9d1d63678fdb6fa39dac31d8f874fc4e1969
carl Ivan Sampan <carlforwebdev@gmail.com>
2023-05-28 22:30:54 +0600

    Implemented gameover and random image.

--- script.js
+++ script.js
@@ -9,23 +9,22 @@ const bgImgElement = document.getElementById('bg-img');
 
 // Set the initial start time
 let start = new Date().getTime();
-
 // Generate a random color code
 function randomColorGen() {
   const letters = '123456789ABCD'.split('');
   let color = '#';
   for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
 
 // Set a random background image class for the game
 function randomImage() {
-  const backgroundClasses = [
-    'background-image1',
-    'background-image2',
-    'background-image3',
+    const backgroundClasses = [
+        'background-image1',
+        'background-image2',
+        'background-image3',
   ];
   const randomIndex = Math.floor(Math.random() * backgroundClasses.length);
   const randomBackgroundClass = backgroundClasses[randomIndex];
@@ -34,39 +33,52 @@ function randomImage() {
 
 // Make the shape appear on the screen
 function makeShapeAppear() {
-  shapeElement.style.display = 'block';
-  start = new Date().getTime();
+    shapeElement.style.display = 'block';
+    start = new Date().getTime();
 }
 
 // Set the shape properties, such as position, size, and color, and schedule the appearance of the shape after a delay
 function appearAfterDelay() {
-  shapeElement.style.display = 'none'; // Hide the shape
-  const left = Math.random() * 80;
-  const top = Math.random() * 70;
-  const width = Math.random() * 200 + 200;
-  const height = Math.random() * 300;
-
-  shapeElement.style.borderRadius = Math.random() > 0.5 ? '50px' : '0%';
-  shapeElement.style.backgroundColor = randomColorGen();
-  shapeElement.style.width = width + 'px';
-  shapeElement.style.height = height + 'px';
-  shapeElement.style.left = left + '%';
-  shapeElement.style.top = top + '%';
-
-  // Schedule the appearance of the shape after a random time delay
-  setTimeout(makeShapeAppear, Math.random() * 2000);
+    const left = Math.random() * 80;
+    const top = Math.random() * 70;
+    const width = Math.random() * 100 + 100;
+    const height = Math.random() * 100 + 100;
+    
+    if(Math.random() > 0.5) {
+        shapeElement.style.borderRadius = '50px';
+        bgImgElement.style.borderRadius = '50px';
+    } else {
+        shapeElement.style.borderRadius = '0%';
+        bgImgElement.style.borderRadius = '0%';
+    }
+    shapeElement.style.backgroundColor = randomColorGen();
+    shapeElement.style.width = width + 'px';
+    shapeElement.style.height = height + 'px';
+    shapeElement.style.left = left + '%';
+    shapeElement.style.top = top + '%';
+    
+    // Schedule the appearance of the shape after a random time delay
+    setTimeout(makeShapeAppear, Math.random() * 2000);
 }
 
 // Handle the click event on the shape element
 function handleClick() {
-  randomImage(); // Change the background image
-  shapeElement.style.display = 'none';
-  const end = new Date().getTime();
-  const timeTaken = (end - start) / 1000;
+    randomImage(); // Change the background image
+    shapeElement.style.display = 'none';
+    const end = new Date().getTime();
+    const timeTaken = (end - start) / 1000;
+    gameover()
   scoreElement.innerHTML = timeTaken + 's';
   appearAfterDelay();
 }
-
+function gameover(){
+    const end = new Date().getTime()
+    const currentTime = (end - start) / 1000
+    if( currentTime > 10){
+        return alert('gameover')
+    }
+    
+}
 // Add a click event listener to the shape element
 shapeElement.onclick = handleClick;
 
@@ -75,3 +87,4 @@ randomImage();
 
 // Initiate the first appearance of the shape
 appearAfterDelay();
+
